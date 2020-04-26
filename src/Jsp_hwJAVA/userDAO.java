3165a02c04cce3920f96f2c497ee45a27d2e7af4
HongJin-Jeong <mistake9518@gmail.com>
2020-04-26 16:08:06 +0900

    6주차 과제 점검2

--- src/Jsp_hwJAVA/userDAO.java
+++ src/Jsp_hwJAVA/userDAO.java
@@ -9,18 +9,22 @@ import java.util.List;
 public class userDAO {
     public static List<User> findAll() throws Exception {
         String sql = "SELECT s.*, d.departmentName " +
-                     "FROM student s LEFT JOIN department d ON s.departmentId = d.id";
+                "FROM student s LEFT JOIN department d ON s.departmentId = d.id";
         try (Connection connection = DB.getConnection("student1");
              PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
             ArrayList<User> list = new ArrayList<User>();
             while (resultSet.next()) {
                 User user = new User();
+                user.setId(resultSet.getInt("id"));
                 user.setUserid(resultSet.getString("Userid"));
+                user.setPassword(resultSet.getString("Password"));
                 user.setName(resultSet.getString("name"));
                 user.setEmail(resultSet.getString("email"));
+                user.setDepartmentId(resultSet.getInt("departmentId"));
                 user.setDepartmentName(resultSet.getString("departmentName"));
                 user.setEnabled(resultSet.getBoolean("enabled"));
+                user.setUserType(resultSet.getString("UserType"));
                 list.add(user);
             }
             return list;
