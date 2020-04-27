e8893042afc120cd8a051a2451b9261765a197b6
HongJin-Jeong <mistake9518@gmail.com>
2020-04-27 11:15:12 +0900

    7주차 과제 1

---
+++ src/Jsp_hwJAVA/UserDAO2.java
@@ -0,0 +1,63 @@
+package Jsp_hwJAVA;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO2 {
+    public static List<User> findAll() throws Exception {
+        String sql = "SELECT u.*,d.departmentName " +
+                "FROM user u LEFT JOIN department d ON u.departmentId = d.id";
+        try (Connection connection = DB.getConnection("student1");
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            ArrayList<User> list = new ArrayList<User>();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserid(resultSet.getString("Userid"));
+                user.setPassword(resultSet.getString("Password"));
+                user.setName(resultSet.getString("name"));
+                user.setEmail(resultSet.getString("email"));
+                user.setDepartmentId(resultSet.getInt("departmentId"));
+                user.setDepartmentName(resultSet.getString("departmentName"));
+                user.setEnabled(resultSet.getBoolean("enabled"));
+                user.setUserType(resultSet.getString("UserType"));
+                list.add(user);
+            }
+            return list;
+        }
+    }
+
+
+    public static List<User> findByName(String name) throws Exception {
+        String sql = "SELECT u.*,d.departmentName " +
+                     "FROM user u LEFT JOIN department d ON u.departmentId = d.id " +
+                     "WHERE u.name LIKE ?";
+        try (Connection connection = DB.getConnection("student1");
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, name + "%");
+            try (ResultSet resultSet = statement.executeQuery()) {
+                ArrayList<User> list = new ArrayList<User>();
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getInt("id"));
+                    user.setUserid(resultSet.getString("Userid"));
+                    user.setPassword(resultSet.getString("Password"));
+                    user.setName(resultSet.getString("name"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setDepartmentId(resultSet.getInt("departmentId"));
+                    user.setDepartmentName(resultSet.getString("departmentName"));
+                    user.setEnabled(resultSet.getBoolean("enabled"));
+                    user.setUserType(resultSet.getString("UserType"));
+                    list.add(user);
+                }
+                return list;
+            }
+        }
+    }
+
+
+}
