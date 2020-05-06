87c3c0306d19d76849f47e17cc97e9bb55f0326d
HongJin-Jeong <mistake9518@gmail.com>
2020-05-06 12:20:20 +0900

    중간고사1

---
+++ src/Jsp_hwJAVA/Book.java
@@ -0,0 +1,48 @@
+package Jsp_hwJAVA;
+
+public class Book {
+	int id;
+	String title;
+	String author;
+	String categoryID;
+	int price;
+	String publisher;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategoryID() {
+		return categoryID;
+	}
+	public void setCategoryID(String categoryID) {
+		this.categoryID = categoryID;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+
+}
---
+++ src/Jsp_hwJAVA/BookDAO.java
@@ -0,0 +1,31 @@
+package Jsp_hwJAVA;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDAO {
+	 public static List<Book> findAll() throws Exception {
+	        String sql = "SELECT b.*,c.categoryName " +
+	                "FROM book b LEFT JOIN category c ON b.categoryID = c.id";
+	        try (Connection connection = DB.getConnection("Book");
+	             PreparedStatement statement = connection.prepareStatement(sql);
+	             ResultSet resultSet = statement.executeQuery()) {
+	            ArrayList<Book> list = new ArrayList<Book>();
+	            while (resultSet.next()) {
+	            	Book book = new Book();
+	            	book.setId(resultSet.getInt("id"));
+	            	book.setTitle(resultSet.getString("title"));
+	            	book.setAuthor(resultSet.getString("author"));
+	            	book.setCategoryID(resultSet.getString("categoryId"));
+	            	book.setPrice(resultSet.getInt("price"));
+	            	book.setPublisher(resultSet.getString("publisher"));
+	                list.add(book);
+	            }
+	            return list;
+	        }
+	    }
+
+}
