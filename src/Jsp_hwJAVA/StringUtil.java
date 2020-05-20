c0e09812ff25952e86c0f85d30f5440414f4acc5
HongJin-Jeong <mistake9518@gmail.com>
2020-05-20 14:41:44 +0900

    과제 중간 체크

---
+++ src/Jsp_hwJAVA/StringUtil.java
@@ -0,0 +1,10 @@
+package Jsp_hwJAVA;
+
+public class StringUtil {
+    public static boolean isEmptyOrBlank(String s) {
+        return s == null ||
+               s.isEmpty() ||
+               s.trim().length() == 0;
+    }
+
+}
---
+++ src/Jsp_hwJAVA/professorService.java
@@ -0,0 +1,38 @@
+package Jsp_hwJAVA;
+
+public class professorService {
+    static final String 학번_필수 = "학번을 입력하세요";
+    static final String 이름_필수 = "이름을 입력하세요";
+    static final String 학년_필수 = "학년을 입력하세요";
+    static final String 학번_중복 = "학번이 중복됩니다";
+
+
+    public static String validate(Student student) throws Exception {
+        if (StringUtil.isEmptyOrBlank(student.getStudentNumber()))
+            return 학번_필수;
+        if (StringUtil.isEmptyOrBlank(student.getName()))
+            return 이름_필수;
+        if (student.getYear() == 0)
+            return 학년_필수;
+        Student student1 = professorDAO.findByStudentNumber(student.getStudentNumber());
+        if (student1 != null && student1.getId() != student.getId())
+            return 학번_중복;
+        return null;
+    }
+
+    public static String insert(Student student) throws Exception {
+        String errorMessage = validate(student);
+        if (errorMessage != null) return errorMessage;
+        professorDAO.insert(student);
+        return null;
+    }
+
+    public static String update(Student student) throws Exception {
+        String errorMessage = validate(student);
+        if (errorMessage != null) return errorMessage;
+        professorDAO.update(student);
+        return null;
+    }
+
+
+}
